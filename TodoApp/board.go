4aa50a126167c1bdaa65f8d1eab133fc5860356f
DKeshavarz <dankeshavarz1075@gmail.com>
2025-04-25 23:57:17 +0330

    Chore : add board functioalyties templates

---
+++ TodoApp/board.go
@@ -0,0 +1,13 @@
+package TodoApp
+
+func addBoard(inputs []string)(response string, err error){
+	return //TODO: 
+} 
+
+func showBoard(inputs []string)(response string, err error){
+	return //TODO: 
+} 
+
+func deleteBoard(inputs []string)(response string, err error){
+	return //TODO: 
+} 
